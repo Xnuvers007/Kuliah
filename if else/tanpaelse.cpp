ce2f43eeca07b025ad4413c82a875d1a532ec94c
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2022-05-09 18:17:38 +0700

    Create tanpaelse.cpp

---
+++ if else/tanpaelse.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Masukan Nilai N : ";
+    cin>>n;
+
+    if(n>50)
+    {
+        n=n+10;
+        cout<<"Nilai Nya N Adalah : "<<n;
+    }
+    return 0;
+}
