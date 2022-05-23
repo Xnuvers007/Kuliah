655f63f0104575960f64cda332183bb8a4caa93c
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2022-05-23 18:13:54 +0700

    Update MencariPosisiIndexArray.cpp

--- array/MencariPosisiIndexArray.cpp
+++ array/MencariPosisiIndexArray.cpp
@@ -8,13 +8,18 @@ int main() {
                      {1, 2, 32, 23, 63},
                      {3, 4, 22, 34, 74}
                     };
-    int target = 63;
-    int row = 0;
-    int col = 0;
+                                    // 2 adalah baris 5 adalah kolom
+                                    // {1, 2, 3},
+                                    // {4, 5, 6};
+                                    // {7, 8, 9};
+                                    // ini adalah contoh array dengan 3 baris dan 3 kolom
+    int target = 63;    // int target = 3;
+    int row = 0;       // ini baris
+    int col = 0;        // ini kolom
     bool found = false;
-    while (row < 5 && !found) {
+    while (row < 5 && !found) { // while (row < 3 && !found)
         col = 0;
-        while (col < 5 && !found) {
+        while (col < 5 && !found) { // while (col < 2 && !found)
             if (arr[row][col] == target) {
                 found = true;
             } else {
@@ -24,9 +29,9 @@ int main() {
         row++;
     }
     if (found) {
-        cout << "Found " << target << " at " << row << "," << col << endl;
+        cout << target << " Berhasil Ditemukan " << "pada Index " << row << "," << col << endl;
     } else {
-        cout << "Not found" << endl;
+        cout << "Tidak berhasil Ketemu :(" << endl;
     }
     return 0;
 }
