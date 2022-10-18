648527d4f4e771320a6b975c7b8dbde33d342742
LuckySora007 <indra111.ida@gmail.com>
2022-10-18 20:32:33 +0700

    Add latihan 8

---
+++ Semester 2/pertemuan 8/lat8_1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define SIZE 12
+main()
+{
+ int a [ SIZE ]= {1, 3, 5, 4, 7, 2, 99, 16, 45, 67, 89, 45};
+ int indeks, total = 0;
+ for (indeks=0 ; indeks<=SIZE-1 ; indeks++)
+ {
+total += a [indeks];
+ }
+ printf ("\nTotal Setiap Elemen Array adalah %d" , total);
+ return 0;
+}
