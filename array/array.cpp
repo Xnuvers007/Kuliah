8b4e7c6251458450e301a3065c9c122f6ac2b547
LuckySora007 <indra111.ida@gmail.com>
2022-10-31 09:57:35 +0700

    tambah array1.cpp

---
+++ array/array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    char indra[] = {'I', 'N', 'D', 'R', 'A'};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << indra[i] << endl;
+    }
+}
