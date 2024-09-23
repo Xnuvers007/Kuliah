1950fd757e1b2c4b24cc268a9db8d8948026fc1f
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2024-09-23 19:51:22 +0700

    Update MainPanel.java

--- semester 6/komputer grafik/latihangui/LatGui/src/main/java/com/pertemuan2/latgui/MainPanel.java
+++ semester 6/komputer grafik/latihangui/LatGui/src/main/java/com/pertemuan2/latgui/MainPanel.java
@@ -1,6 +1,8 @@
 package com.pertemuan2.latgui;
 
 import java.awt.*;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 import javax.swing.*;
 /**
  *
@@ -17,11 +19,21 @@ public class MainPanel extends JPanel{
      * @param g
      */
     @Override
-    public void paintComponent(Graphics g)
-    {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D)g;
-        g2.drawRect(10,10,40,50);
-        g2.drawString("Komputer Grafik Latihan 1", 10, 70);
-    }
-}
+//  public void paintComponent(Graphics g){
+//  //paintcomponentdengandefaultmethod
+//  super.paintComponent(g);
+// 
+// Graphics2D g2=(Graphics2D)g;
+// g2.drawRect(10,10,40,50);
+// g2.drawString("Komputer Grafik Latihan1",10,70);
+// }
+  public void paintComponent(Graphics g){
+      super.paintComponent(g);
+      Graphics2D g2 = (Graphics2D) g;
+      g2.drawLine(10, 10, 10, 100);
+      Point2D titik1 = new Point2D.Double(10,10);
+      Point2D titik2 = new Point2D.Double(100,10);
+      Line2D garis2 = new Line2D.Double(titik1, titik2);
+      g2.draw(garis2);
+  }
+ }
