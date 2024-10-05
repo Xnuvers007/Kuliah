3133e54a13ae3d32b6c26fbffd52fcb8c6876981
Xnuvers007 <indra111.ida@gmail.com>
2024-10-05 16:17:07 +0700

    gapapa

--- semester 6/komputer grafik/latihangui/komputergrafik/src/main/java/elearning/tugaspertemuan5/komputergrafik/GambarLingkaran.java
+++ semester 6/komputer grafik/latihangui/komputergrafik/src/main/java/elearning/tugaspertemuan5/komputergrafik/GambarLingkaran.java
@@ -10,48 +10,33 @@ public class GambarLingkaran extends JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
-        // Mengatur warna latar belakang
         g2d.setColor(Color.WHITE);
         g2d.fillRect(0, 0, getWidth(), getHeight());
 
-        // Menggambar garis kartesius
         g2d.setColor(Color.GRAY);
         int centerX = getWidth() / 2;
         int centerY = getHeight() / 2;
 
-        // Garis horizontal (sumbu X)
         g2d.drawLine(0, centerY, getWidth(), centerY);
-        // Garis vertikal (sumbu Y)
         g2d.drawLine(centerX, 0, centerX, getHeight());
 
-        // Menambahkan angka pada sumbu X dan Y
         g2d.setFont(new Font("Arial", Font.PLAIN, 12));
-        
-        // Menambahkan angka pada sumbu X dari -10 hingga 10
+
         for (int i = -10; i <= 10; i++) {
-            // Sumbu X
             g2d.drawString(String.valueOf(i), centerX + i * 20 - 5, centerY + 15);
-            // Sumbu Y
             g2d.drawString(String.valueOf(i), centerX - 25, centerY - i * 20 + 5);
         }
 
-        // Mengatur warna untuk lingkaran
         g2d.setColor(Color.BLUE);
-
-        // Jari-jari lingkaran
-        int r = 6 * 20; // Mengalikan dengan faktor skala agar terlihat lebih besar
-
-        // Menggambar lingkaran
+        int r = 6 * 20;
         g2d.drawOval(centerX - r, centerY - r, 2 * r, 2 * r);
 
-        // Menggambar titik-titik di kuadran pertama
         g2d.setColor(Color.RED);
         for (int x = 0; x <= 6; x++) {
-            int y = (int) Math.sqrt(r * r - x * x); // Menghitung y
-            g2d.fillOval(centerX + x * 20 - 2, centerY - y * 20 - 2, 4, 4); // Menggambar titik
+            int y = (int) Math.sqrt(r * r - x * x);
+            g2d.fillOval(centerX + x * 20 - 2, centerY - y * 20 - 2, 4, 4);
         }
 
-        // Menambahkan koordinat titik
         g2d.setColor(Color.BLACK);
         for (int x = 0; x <= 6; x++) {
             int y = (int) Math.sqrt(r * r - x * x);
